3eece16f39fdb2ea1c22daa4188597decfe7ab94
Stanislaw Cruz <stanycruz@outlook.com>
2022-11-15 14:07:25 -0300

    refactor: move getUser to actions file

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -3,20 +3,28 @@ import { useEffect, useContext } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
 import RepoList from '../components/repos/RepoList'
+import { getUser, getUserRepos } from '../context/github/GithubActions'
 
 import GithubContext from '../context/github/GithubContext'
 
 function User() {
-  const { getUser, user, loading, getUserRepos, repos } =
+  const { user, loading, repos, dispatch } =
     useContext(GithubContext)
 
   const params = useParams()
 
   useEffect(() => {
-    getUser(params.login)
-    getUserRepos(params.login)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    dispatch({ type: 'SET_LOADING' })
+    const getUserData = async() => {
+      const userData = await getUser(params.login)
+      dispatch({ type: 'GET_USER', payload: userData })
+
+      const userRepoData = await getUserRepos(params.login)
+      dispatch({ type: 'GET_REPOS', payload: userRepoData })
+    }
+
+    getUserData()
+  }, [dispatch, params.login])
 
   const {
     name,
